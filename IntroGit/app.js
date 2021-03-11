c6206e4f3d405cd0cde2eca1df184ac7b2ac3d4c
LuisMi99 <l17201045@pachuca.tecnm.mx>
2021-03-11 10:16:48 -0600

    cambio de carpeta

--- IntroGit/app.js
+++ IntroGit/app.js
@@ -1,3 +1,3 @@
 let tuitulo=document.getElementById('titulo');
 
-tuitulo.innerHTML="cambio en el titulo";
+tuitulo.innerHTML="Luis Miguel Castillo Olvera está aprendiendo GitHub por linea de comandos";
